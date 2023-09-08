9f12467db0369e1aaf927e069fbe59eaaa924c6f
Igor2698 <128493043+Igor2698@users.noreply.github.com>
2023-09-08 13:47:51 +0300

    +

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,14 +8,7 @@ import Filter from '../Filter';
 
 const getIntialContacts = () => {
   const savedContacts = localStorage.getItem('contacts');
-  return (
-    JSON.parse(savedContacts) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-  );
+  return JSON.parse(savedContacts) ?? [];
 };
 
 export const App = () => {
